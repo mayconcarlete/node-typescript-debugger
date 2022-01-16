832c5e650654d13622d494dfe459b4130d9267e3
Maycon Carlete <maycon.carlete@gmail.com>
2022-01-15 22:31:52 -0300

    feat: debug docker application with vscode

---
+++ 05-js-with-docker-vscode/index.js
@@ -0,0 +1,15 @@
+debugger
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const multiply = (n1, n2) => n1 * n2
+
+app.post('/', async(req, res) => {
+  const {n1, n2} = req.body
+  const result = multiply(n1, n2)
+  res.status(200).send({ result })
+})
+
+const PORT = 3000
+app.listen(PORT, () => console.log('We are online'))
